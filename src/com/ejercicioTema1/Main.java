2928c4c123ae668a7725a6614a28e46d1bfe207a
Chard33 <jchar33@hotmail.com>
2023-05-05 08:05:34 -0600

    Entrega ejercicios tema 1

---
+++ src/com/ejercicioTema1/Main.java
@@ -0,0 +1,31 @@
+package com.ejercicioTema1;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        byte varByte = 3;
+        short varShort = 33;
+        int varInt = 333;
+        long varLong = 3333;
+
+        float varFloat = 33.33f;
+        double varDouble = 33.33;
+
+        char varChar = 'a';
+
+        boolean varBoolean = true;
+
+        String varString = "Variable cadena de texto";
+
+        System.out.println("Variable byte: " + varByte + "\n" +
+                "Variable short: "  + varShort + "\n" +
+                "Variable int: "  + varInt + "\n" +
+                "Variable long: "  + varLong + "\n" +
+                "Variable float: "  + varFloat + "\n" +
+                "Variable double: "  + varDouble + "\n" +
+                "Variable boolean: "  + varBoolean + "\n" +
+                "Variable char: "  + varChar + "\n" +
+                "Variable String: "  + varString);
+    }
+}
